1b840cd46aa0f50c969078f20ee09ba51d25e04d
Gautam <pandeygautam394@gmail.com>
2026-01-08 10:42:39 +0530

    ChessBoard:

---
+++ Unstop/Day1/CHESSBOARD.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String determineColor(String s) {
+        return (s.charAt(0)+s.charAt(1))%2==1?"White":"Black";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine().trim();
+        String result = determineColor(s);
+        System.out.println(result);
+    }
+}
