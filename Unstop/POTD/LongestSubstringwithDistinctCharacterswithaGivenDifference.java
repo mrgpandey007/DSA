7dc8d235c2e05c33c1de1d412dcf30d8a003ea2a
Gautam <pandeygautam394@gmail.com>
2026-01-08 10:35:16 +0530

    POTD1UNSTOP:

---
+++ Unstop/POTD/LongestSubstringwithDistinctCharacterswithaGivenDifference.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.HashSet;
+public class Main {
+    public static int findLongestDistinctSubstring(int N, int D, String s) {
+        int max=-1;
+        for(int i=0;i<N;i++) {
+            HashSet<Character> count=new HashSet<>();
+
+            for(int j=i;j<N;j++) {
+                if(count.contains(s.charAt(j))) break;
+                count.add(s.charAt(j));
+                if (Math.abs(s.charAt(i) - s.charAt(j)) == D) {
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int D = scanner.nextInt();
+        String S = scanner.next();
+        int result = findLongestDistinctSubstring(N, D, S);
+        System.out.println(result);
+    }
+}
