bbb8d1160698a0b35df352919cb29f61f73180f0
Gautam <pandeygautam394@gmail.com>
2026-01-10 07:37:57 +0530

    Concatenation of Array:

---
+++ Array/ConcatenationofArray.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len=nums.length;
+        int[] ans=new int[2*len];
+        System.arraycopy(nums,0,ans,0,len);
+        System.arraycopy(nums,0,ans,len,len);
+        return ans;
+    }
+}
