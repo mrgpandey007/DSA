137c426ebac0251547723a3910ea93788edbaef4
Gautam <pandeygautam394@gmail.com>
2026-01-11 21:37:52 +0530

    TheInterstellar:

---
+++ Unstop/POTD/TheInterstellarCargoLogistics.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Main {
+    public static int minfuel(int n,int[] w,int k) {
+         if (k > n) {
+            return -1;
+        }
+
+        int[][] dp = new int[k + 1][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, 1000000000);
+        }
+
+       
+        dp[0][0] = 0;
+
+  
+        for (int s = 1; s <= k; s++) {
+            for (int i = 1; i <= n; i++) {
+                int cur = 0;
+
+                for (int p = i - 1; p >= s - 1; p--) {
+                    cur = Math.max(cur, w[p]);
+                    
+                    if (dp[s - 1][p] != 1000000000) {
+                        dp[s][i] = Math.min(dp[s][i], dp[s - 1][p] + cur);
+                    }
+                }
+            }
+        }
+        return dp[k][n];
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] weight = new int[n];
+        for(int i=0; i < n ; i++) {
+            weight[i]=input.nextInt();
+        }
+        int slip = input.nextInt();
+        System.out.println(minfuel(n,weight,slip));
+    }
+}
