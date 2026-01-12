1e147f8c7780cd6442aa82e4c0823fe2cb385b23
Gautam <pandeygautam394@gmail.com>
2026-01-12 23:29:44 +0530

    12102026232910:

---
+++ Unstop/POTD/TheCursedMaze.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static int userLogic(int[][] maze) {
+        int value = 0;
+        for(int[] row : maze) {
+            for(int num:row) {
+                if(num>0) {
+                    value+=num;
+                }
+            }
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[][] maze = new int[N][M];
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < M; ++j) {
+                maze[i][j] = scanner.nextInt();
+            }
+        }
+        // Call user logic function and print the output
+        int result = userLogic(maze);
+        System.out.println(result);
+        scanner.close();
+    }
+}
