0f122a874e4baf14b0d3e551eeaf291e5bc6fb7f
Gautam <pandeygautam394@gmail.com>
2026-01-15 23:00:19 +0530

    15012026230000:

---
+++ Unstop/POTD/ClothStitchingPathOptimization.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static long calculateMinThread(int M, int N, int T) {
+        int turn = 0 ;
+        if(M>1 && N>1) turn = 2* (Math.min(M,N)-1);
+        return M*N+turn*T;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int M = scanner.nextInt(),N = scanner.nextInt(),T = scanner.nextInt();
+        System.out.println(calculateMinThread(M, N, T));
+    }
+}
