7f1da1af9ba9a1786b75f402d953618ec8059d8f
Gautam <pandeygautam394@gmail.com>
2026-01-16 22:29:53 +0530

    160102026223:

---
+++ Unstop/POTD/MedicineMixing.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static long minEnergyCost(int[] T, int K) {
+        int N = T.length;
+        int[] A = T.clone();
+        for (int i = 1; i < N; i++) {
+            A[i] = Math.min(A[i], A[i - 1] + K);
+        }
+        for (int i = N - 2; i >= 0; i--) {
+            A[i] = Math.min(A[i], A[i + 1] + K);
+        }
+        long cost = 0;
+        for (int i = 0; i < N; i++) {
+            cost += (T[i] - A[i]);
+        }
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+
+        int[] T = new int[N];
+        for (int i = 0; i < N; i++) {
+            T[i] = scanner.nextInt();
+        }
+
+        long result = minEnergyCost(T, K);
+        System.out.println(result);
+    }
+}
