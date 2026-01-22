2e2ca4eb1fe1fa6f143b7b0d1b2c6beafdaab582
Gautam <pandeygautam394@gmail.com>
2026-01-22 22:30:01 +0530

    220120232230:

---
+++ Unstop/POTD/WaterSlideCongestionManagement.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static int maxTotalPopularity(int[] P, int N) {
+        if (N == 0) return 0;
+        int prev2 = 0;                  
+        int prev1 = 0;  
+        for (int i = 0; i < N; i++) {
+            int temp = Math.max(prev1, prev2 + P[i]);
+            prev2 = prev1;
+            prev1 = temp;
+        }
+        return prev1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] P = new int[N];
+        for (int i = 0; i < N; i++) {
+            P[i] = sc.nextInt();
+        }
+        System.out.println(maxTotalPopularity(P, N));
+        sc.close();
+    }
+}
