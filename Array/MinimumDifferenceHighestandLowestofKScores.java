770c901aacc931f1b1c28171be8788756b5f3d32
Gautam <pandeygautam394@gmail.com>
2026-01-25 20:03:42 +0530

    MinimumDifferenceHighestandLowestofKScores:

---
+++ Array/MinimumDifferenceHighestandLowestofKScores.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if(k==1) return 0;
+        Arrays.sort(nums);
+        int n = nums.length;
+        if(k==n) return nums[n-1]-nums[0];
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<=n-k; i++) {
+            min=Math.min(min,nums[i+k-1]-nums[i]);
+        }
+        return min;
+    }
+}
